590c9f44e19341f5a06d3871dbbd22ccc6978256
donishabduvaliyev <donishkratos413677@gmail.com>
2025-08-14 03:33:18 +0500

    home-page-search-fix

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,13 +1,44 @@
 import React from "react";
+const SearchIcon = ({ className = "w-5 h-5" }) => (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className={className}> <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" /> </svg>);
+import { useState, useEffect, useRef } from "react";
+import { useTranslation } from "react-i18next";
+import { useAppContext } from "../context/context";
+
+
 
 const SearchBar = () => {
+  const { navigate } = useAppContext();
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleHomeSearch = (e) => {
+    e.preventDefault();
+    if (searchTerm.trim()) {
+      navigate(`/search?query=${encodeURIComponent(searchTerm.trim())}`);
+    }
+  };
+
+
+
+
   return (
-    <div className="top-0 left-0 w-full  z-50 shadow p-4">
-      <input
-        type="text"
-        placeholder="Search barbershops, salons..."
-        className="w-full p-3 bg-white/70 dark:bg-gray-700/90 backdrop-blur-xl rounded-2xl shadow-2xl shadow-black/20 border border-white/20 focus:ring-2 focus:ring-yellow-200"
-      />
+    <div className="relative flex-grow">
+      <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"><SearchIcon className="w-5 h-5 text-yellow-400" /></div>
+      <form action="" onSubmit={handleHomeSearch}>
+
+
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder={t('Search for salons, spas, etc.')}
+          className="w-full pl-10 pr-4 py-3 bg-gray-200 dark:bg-zinc-900 border-transparent rounded-xl focus:ring-2 focus:ring-purple-500 focus:border-transparent"
+        />
+        <button type="submit">Search</button>
+
+      </form>
+
+
     </div>
   );
 };
